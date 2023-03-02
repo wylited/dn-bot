bb407328472656cd9bb02f650f50a311fadb913c
wylited <wylited@gmail.com>
2023-03-02 20:15:29 +0800

    init

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+#include <dpp/dpp.h>
+
+// possibly only for development
+#include <bits/stdc++.h>
+
+// dont use a namspace for now
+// using namepsace std;
+
+    
+int main() {
+    std::string BOT_TOKEN;
+
+    std::ifstream inFile;
+    inFile.open("token.env");
+
+    getline(inFile, BOT_TOKEN);
+    inFile.close();
+
+    dpp::cluster bot(BOT_TOKEN);
+    
+    bot.on_log(dpp::utility::cout_logger());
+    
+    bot.on_slashcommand([](const dpp::slashcommand_t& event) {
+            if (event.command.get_command_name() == "ping") {
+            event.reply("Pong!");
+        }
+    });
+    
+    bot.on_ready([&bot](const dpp::ready_t& event) {
+        if (dpp::run_once<struct register_bot_commands>()) {
+            bot.global_command_create(
+                dpp::slashcommand("ping", "Ping pong!", bot.me.id)
+            );
+        }
+    });
+    
+    bot.start(dpp::st_wait);
+}
